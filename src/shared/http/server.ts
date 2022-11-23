7515f629f74f7b3e54978a09483b7eaf8679431b
Carlos Eduardo Andrade Iatskiu <carlosiatskiu@MacBook-Pro-de-Carlos.local>
2022-11-23 14:00:03 -0300

    conf

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,29 @@
-import express from 'express';
+import 'reflect-metadata'; //primeira linha sempre!
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((error: Error, request: Request, 
+    response: Response, next: NextFunction) =>{
+        if(error instanceof AppError){
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message
+            });
+        }
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal Server Error'
+        });
+    });
 
 app.listen(3333, () => {
     console.log('Server Started on Port 3333!');
 })
+
