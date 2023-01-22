ffbc124f17183d8b5cd7d5b60e86e8481c79483a
Carlos Eduardo Andrade Iatskiu <carlosiatskiu@MacBook-Pro-de-Carlos.local>
2023-01-22 09:31:21 -0300

    pre-ferias

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,14 +1,32 @@
 import { Router } from "express";
 import ProductsController from "../controllers/ProductsController";
+import { celebrate, Joi, Segments } from "celebrate";
 
 const productRouter = Router();
 const productsController = new ProductsController();
 
 productRouter.get('/', productsController.index);
-productRouter.get('/:id', productsController.show);
-productRouter.post('/', productsController.create);
-productRouter.put('/:id', productsController.update);
-productRouter.delete('/:id', productsController.delete);
+productRouter.get('/:id', celebrate({
+    [Segments.PARAMS] : {id: Joi.string().uuid().required()}
+}), productsController.show);
+productRouter.post('/', celebrate({
+   [Segments.BODY]: {
+    name: Joi.string().required(),
+    price: Joi.number().precision(2).required(),
+    quantity: Joi.number().required()
+   } 
+}), productsController.create);
+productRouter.put('/:id', celebrate({
+    [Segments.PARAMS] : {id: Joi.string().uuid().required()},
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required()
+       } 
+}), productsController.update);
+productRouter.delete('/:id', celebrate({
+    [Segments.PARAMS] : {id: Joi.string().uuid().required()}
+}), productsController.delete);
 
 export default productRouter;
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata'; //primeira linha sempre!
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 app.use((error: Error, request: Request, 
     response: Response, next: NextFunction) =>{
         if(error instanceof AppError){
